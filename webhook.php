caafebf8cd0ba5e33379f271e7d3a853a96fbe67
Kyle Pamintuan <kylepamintuan@gmail.com>
2018-10-04 18:01:16 -0700

    updated webhook.php with sample query.

---
+++ webhook.php
@@ -0,0 +1,9 @@
+<?php
+
+require_once('credentials.php');
+
+$query = "INSERT INTO receive_email SET subject='{$_POST['subject']}', body='{$_POST['email']}', sender='{$_POST['from']}', recipient='{$_POST['to']}', sent=NOW()";
+
+mysqli_query($conn, $query);
+
+?>
