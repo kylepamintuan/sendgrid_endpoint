fa165bbb95a7b4ce22a9e7266e891c0d38b19bfb
Kyle Pamintuan <kylepamintuan@gmail.com>
2018-10-04 18:29:22 -0700

    fixed body message post error.

--- webhook.php
+++ webhook.php
@@ -2,7 +2,7 @@
 
 require_once('credentials.php');
 
-$query = "INSERT INTO receive_email SET subject='{$_POST['subject']}', body='{$_POST['email']}', sender='{$_POST['from']}', recipient='{$_POST['to']}', sent=NOW()";
+$query = "INSERT INTO receive_email SET subject='{$_POST['subject']}', body='{$_POST['html']}', sender='{$_POST['from']}', recipient='{$_POST['to']}', sent=NOW()";
 
 mysqli_query($conn, $query);
 
